dc35e905915047150a82f10ffe6e6c481c0ad7ea
mutiaraarta <mumuy3006@gmail.com>
2025-09-29 13:03:39 +0700

    test

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,7 +16,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        if (config('app.env') === 'production') {
+        URL::forceScheme('https');
+        }
     }
+    
 
     /**
      * Register any application services.
